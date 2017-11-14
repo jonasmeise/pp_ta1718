0927b1a272929670a0a2cf41d05c4a5f20eaa701
jonasmeise <32752445+jonasmeise@users.noreply.github.com>
2017-11-14 17:00:24 +0100

    changed to stanfordnlp, still error remaining on pipe

--- src/main/java/project/Pipeline.java
+++ src/main/java/project/Pipeline.java
@@ -1,12 +1,17 @@
 package project;
 
-import org.apache.uima.fit.factory.AnalysisEngineFactory;
-import org.apache.uima.fit.factory.CollectionReaderFactory;
+import static org.apache.uima.fit.factory.CollectionReaderFactory.createReader;
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.collection.CollectionReader;
 import org.apache.uima.fit.pipeline.SimplePipeline;
 import org.apache.uima.fit.component.CasDumpWriter;
 
+import de.tudarmstadt.ukp.dkpro.core.io.text.TextReader;
+import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordNamedEntityRecognizer;
 import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
-import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpPosTagger;
+import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordParser;
+import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordSegmenter;
 import de.unidue.langtech.teaching.pp.example.BaselineExample;
 import de.unidue.langtech.teaching.pp.example.EvaluatorExample;
 import de.unidue.langtech.teaching.pp.example.ReaderExample;
@@ -17,16 +22,20 @@ public class Pipeline
     public static void main(String[] args)
         throws Exception
     {    	
-        SimplePipeline.runPipeline(
-                CollectionReaderFactory.createReader(
-                        ReaderExample.class,
-                        ReaderExample.PARAM_INPUT_FILE, "src/test/resources/test/input2.txt"
-                ),
-                AnalysisEngineFactory.createEngineDescription(BaselineExample.class),
-                AnalysisEngineFactory.createEngineDescription(EvaluatorExample.class),
-                AnalysisEngineFactory.createEngineDescription(BreakIteratorSegmenter.class, BreakIteratorSegmenter.PARAM_LANGUAGE, "en"),
-                AnalysisEngineFactory.createEngineDescription(OpenNlpPosTagger.class, OpenNlpPosTagger.PARAM_LANGUAGE, "en"),
-                AnalysisEngineFactory.createEngineDescription(CasDumpWriter.class)
-        );
+    	
+    	CollectionReader reader = createReader(
+    	        TextReader.class,
+    	        TextReader.PARAM_SOURCE_LOCATION, ".\\src\\test\\resources\\test\\input2.txt",
+    	        TextReader.PARAM_LANGUAGE, "en");
+    	
+        AnalysisEngineDescription seg = createEngineDescription(BreakIteratorSegmenter.class);
+
+        AnalysisEngineDescription ner = createEngineDescription(StanfordNamedEntityRecognizer.class);
+
+        AnalysisEngineDescription parser = createEngineDescription(StanfordParser.class);
+
+        AnalysisEngineDescription writer = createEngineDescription(CasDumpWriter.class);
+    	
+    	SimplePipeline.runPipeline(reader, seg, ner, parser, writer);
     }
 }
